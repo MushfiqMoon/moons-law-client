26f007eb2f6f10f5a6f3c0f1dcb553cfef135d36
MushfiqMoon <mushfiq.moon013@google.com>
2022-11-09 15:43:30 +0600

    auto review updated

--- moons-law/src/components/ServiceSingle.js
+++ moons-law/src/components/ServiceSingle.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import toast from 'react-hot-toast';
 import { Link, useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthProvider';
@@ -6,8 +6,9 @@ import { AuthContext } from '../contexts/AuthProvider';
 const ServiceSingle = () => {
 
     const { user } = useContext(AuthContext);
+    // const reviewData =  useLoaderData();
     const { imageUrl, name, message, price, _id } = useLoaderData();
-
+    const [reviews, setReviews] = useState([])
 
     const handleReview = e => {
         e.preventDefault()
@@ -15,15 +16,15 @@ const ServiceSingle = () => {
 
         const review = {
             serviceName: name,
-            serviceId: _id,
+            sid: _id,
             reviewMessage: e.target.review.value,
             reviewerName: user?.displayName,
+            rmail: user?.email,
             reviewerphotoUrl: user?.photoURL,
         }
-        console.log(review)
-        // sending the data to server
 
-        fetch('http://localhost:5000/reviews', {
+        // sending the data to server
+        fetch('https://b6a11-service-review-server-side-mushfiq-moon.vercel.app/reviews', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -32,13 +33,30 @@ const ServiceSingle = () => {
         })
             .then((res) => res.json())
             .then((data) => {
-                console.log(data)
+                console.log("from post",data)
                 if (data.acknowledged) {
                     toast.success('Review Added')
                     form.reset()
+                    // return
                 }
+                const newReview = [review,...reviews]
+                setReviews(newReview)
+
             })
     }
+
+    // Receiving service based review 
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews/?sid=${_id}`)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                setReviews(data)
+            })
+            .catch(err => console.error(err))
+
+    }, [_id])
 
 
     return (
@@ -48,7 +66,7 @@ const ServiceSingle = () => {
                     <div className="flex py-12 md:flex-row flex-col items-center">
                         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
                             <img className="object-cover object-center rounded" src={imageUrl ? imageUrl : 'https://dummyimage.com/720x600'} alt={name} />
-                            <h3 className='text-xl text-primary font-medium title-font my-4'>Price Starts from - ${price}</h3>
+                            <h3 className='text-2xl text-primary font-medium title-font my-4'>Price Starts from - ${price}</h3>
                         </div>
                         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{name}</h1>
@@ -60,7 +78,7 @@ const ServiceSingle = () => {
                 {
                     !user?.uid
                         ?
-                        <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">Please <Link to='/login' className='text-primary'>login</Link> to add a review.</h2>
+                        <h2 className="text-3xl font-medium text-gray-900 title-font mb-2 py-10">Please <Link to='/login' className='text-primary'>login</Link> to add a review.</h2>
                         :
                         <section className="text-gray-600 body-font bg-gray-100 p-20">
                             <div className="flex md:flex-row flex-col items-center">
@@ -86,35 +104,40 @@ const ServiceSingle = () => {
                                 </div>
                             </div>
                         </section>
+
                 }
 
-                <section className="text-gray-600 body-font overflow-hidden">
-                    <div className='py-24'>
-                        <div className="-my-8 divide-y-2 divide-gray-100">
-                            <div className="py-8 flex flex-wrap md:flex-nowrap">
-                                <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                                    <a className="inline-flex items-center mr-10">
-                                        {
-                                            user?.uid
-                                                ?
-                                                <img alt="blog" src={user?.photoURL ? user?.photoURL : 'https://placeimg.com/80/80/people'} className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
-                                                :
-                                                <img alt="blog" src='https://placeimg.com/80/80/people' className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
-                                        }
+                {
+                    reviews.length
+                        ?
+                        reviews.map((review, index) =>
 
-                                        <span className="flex-grow flex flex-col pl-4">
-                                            <span className="title-font font-medium text-gray-900">Holden Caulfield</span>
-                                        </span>
-                                    </a>
+                            <section key={index} className="text-gray-600 body-font overflow-hidden">
+                                <div className='py-12'>
+                                    <div className="-my-8 divide-y-2 divide-gray-100">
+                                        <div className="p-8 flex flex-wrap md:flex-nowrap">
+                                            <div className="md:w-64 md:mb-0 mb-6 mr-20 flex-shrink-0 flex flex-col">
+                                                <a className="inline-flex items-center">
+                                                    <img alt="blog" src={review ? review?.reviewerphotoUrl : 'https://placeimg.com/80/80/people'} className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
+                                                    <span className="flex-grow flex flex-col pl-4">
+                                                        <span className="title-font font-medium text-gray-900">{review ? review?.reviewerName : "Anonymous User"}</span>
+                                                        <span className="mt-1 text-gray-500 text-sm">{review?.rmail}</span>
+                                                    </span>
+                                                </a>
+                                            </div>
+                                            <div className="md:flex-grow">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036"><path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path></svg>
+                                                <p className="leading-relaxed">{review?.reviewMessage}</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="md:flex-grow">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036"><path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path></svg>
-                                    <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                            </section>
+                        )
+                        :
+                        <p className="text-center py-20 text-xl">No Review Yet for this Service 😟
+                        </p>
+                }
             </div>
         </>
     )
